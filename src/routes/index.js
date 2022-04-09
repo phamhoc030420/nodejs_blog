bdb986eee690a80fac8d2aa89494a1bb0073501f
phamhoc030420 <ctyhoc@gmail.com>
2022-04-09 16:24:08 +0700

    controller and router

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+const route = (app) => {
+    // app.get('/', (req, res) => {
+    //     res.render('home');
+    // });
+
+    // app.get('/news', (req, res) => {
+    //     res.render('news');
+    // });=>
+    app.use('/news', newsRouter);
+    // app.get('/search', (req, res) => {
+    //     res.render('search');
+    // });
+
+
+    app.use('/', siteRouter);
+
+}
+module.exports = route;
